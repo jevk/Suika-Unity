7f3eecca40e88c67ffc115eac623b61bba144e79
jevk <jevk@users.noreply.github.com>
2023-12-04 03:39:00 +0200

    AAAAAAAA
    + Cooldown added
    + Scoring added

--- Assets/Scripts/CombineCheck.cs
+++ Assets/Scripts/CombineCheck.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class CombineCheck : MonoBehaviour
@@ -6,19 +7,37 @@ public class CombineCheck : MonoBehaviour
     public GameObject[] fruitPrefabs;
 
     private bool isMerging = false;
+    private Coroutine mergingCoroutine;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!isMerging && collision.gameObject.name == transform.gameObject.name)
         {
             isMerging = true;
+            // increase the score on the "score" TextMeshPro gameobject by the index of the new fruit
+            GameObject scoreText = GameObject.Find("score");
+            if (scoreText != null)
+            {
+                TextMeshProUGUI scoreTextMesh = scoreText.GetComponent<TextMeshProUGUI>();
+                if (scoreTextMesh != null)
+                {
+                    int fruitIndex = System.Array.FindIndex(this.fruitPrefabs, fruit => fruit.name == transform.gameObject.name);
+                    scoreTextMesh.text = (int.Parse(scoreTextMesh.text) + fruitIndex + 1).ToString();
+                }
+            }
+
 
             // Determine the master fruit based on their unique identifiers
             CombineCheck masterFruit = DetermineMasterFruit(collision.gameObject.GetComponent<CombineCheck>());
 
             if (masterFruit == this)
             {
-                StartCoroutine(masterFruit.HandleCollision(collision));
+                if (mergingCoroutine != null)
+                {
+                    StopCoroutine(mergingCoroutine);
+                }
+
+                mergingCoroutine = StartCoroutine(HandleCollision(collision));
             }
         }
     }
@@ -30,27 +49,44 @@ public class CombineCheck : MonoBehaviour
         return (GetInstanceID() > otherCombineCheck.GetInstanceID()) ? this : otherCombineCheck;
     }
 
-    public IEnumerator HandleCollision(Collision2D collision)
+    private IEnumerator HandleCollision(Collision2D collision)
     {
         int fruitIndex = System.Array.FindIndex(this.fruitPrefabs, fruit => fruit.name == transform.gameObject.name);
 
         if (fruitIndex < this.fruitPrefabs.Length - 1)
         {
             Destroy(collision.gameObject);
-            GameObject fruit = Instantiate(this.fruitPrefabs[fruitIndex + 1], transform.position, Quaternion.identity);
-            fruit.name = this.fruitPrefabs[fruitIndex + 1].name;
+            GameObject newFruit = Instantiate(this.fruitPrefabs[fruitIndex + 1], transform.position, Quaternion.identity);
+            newFruit.name = this.fruitPrefabs[fruitIndex + 1].name;
 
             // Make sure the rigidbody is not kinematic
-            fruit.GetComponent<Rigidbody2D>().isKinematic = false;
+            newFruit.GetComponent<Rigidbody2D>().isKinematic = false;
 
             // Delay the destruction of the current fruit
             yield return new WaitForSeconds(0.1f); // Adjust the delay as needed
             Destroy(gameObject);
 
             // Play the sound effect "pop.wav" in the "Audio Source" component in the "Spawner" gameobject
-            GameObject.Find("Spawner").GetComponent<AudioSource>().Play();
+            GameObject spawner = GameObject.Find("Spawner");
+            if (spawner != null)
+            {
+                AudioSource audioSource = spawner.GetComponent<AudioSource>();
+                if (audioSource != null)
+                {
+                    audioSource.Play();
+                }
+            }
         }
 
         isMerging = false;
+    }
+
+    private void OnDestroy()
+    {
+        // Stop the coroutine when the script is destroyed
+        if (mergingCoroutine != null)
+        {
+            StopCoroutine(mergingCoroutine);
+        }
     }
 }
--- Assets/Scripts/FruitSpawner.cs
+++ Assets/Scripts/FruitSpawner.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FruitSpawner : MonoBehaviour
@@ -8,11 +7,17 @@ public class FruitSpawner : MonoBehaviour
     bool fruitExists = false;
     public GameObject[] fruitPrefabs;
     string currentFruitName = "ReleasedFruit";
+
+    // Cooldown timer
+    float spawnCooldown = 0f;
+    float spawnCooldownDuration = 1f; // 1 second cooldown
+
     // Start is called before the first frame update
     void Start()
     {
         fruitExists = false;
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -25,10 +30,10 @@ public class FruitSpawner : MonoBehaviour
             mousePos.x = GameObject.Find("RBOUND").transform.position.x;
         transform.position = new Vector3(mousePos.x, transform.position.y, transform.position.z);
 
-        // If fruit does not exist, wait 2 seconds and spawn a random fruit prefab at the "Spawner" position from "fruitPrefabs" as "Fruit" (Cherry, Strawberry, Grapes, Dekopon, or Orange)
+        // If the cooldown is over and fruit does not exist, spawn a random fruit prefab at the "Spawner" position from "fruitPrefabs" as "Fruit" (Cherry, Strawberry, Grapes, Dekopon, or Orange)
         // If a fruit exists already, move the fruit to the "Spawner" position
 
-        if (!fruitExists)
+        if (spawnCooldown <= 0f && !fruitExists)
         {
             // spawn a random fruit prefab at the "Spawner" position from "fruitPrefabs" as "Fruit" (Cherry, Strawberry, Grapes, Dekopon, or Orange
             int fruitIndex = Random.Range(0, fruitPrefabs.Length);
@@ -44,22 +49,34 @@ public class FruitSpawner : MonoBehaviour
             fruit.name = "Fruit";
             fruit.GetComponent<Rigidbody2D>().isKinematic = true;
             fruitExists = true;
+
+            // Reset the cooldown timer
+            spawnCooldown = spawnCooldownDuration;
         }
         else
         {
+            // Reduce the cooldown timer
+            spawnCooldown -= Time.deltaTime;
+
             GameObject fruit = GameObject.Find("Fruit");
-            fruit.transform.position = transform.position;
+            if (fruit != null)
+            {
+                fruit.transform.position = transform.position;
+            }
         }
 
         // On mouse click, release the fruit and name it to the current fruit name
         if (Input.GetMouseButtonDown(0))
         {
             GameObject fruit = GameObject.Find("Fruit");
-            fruit.name = currentFruitName;
-            fruit.GetComponent<Collider2D>().enabled = true;
-            fruit.GetComponent<Rigidbody2D>().isKinematic = false;
-            fruit.layer = 6;
-            fruitExists = false;
+            if (fruit != null)
+            {
+                fruit.name = currentFruitName;
+                fruit.GetComponent<Collider2D>().enabled = true;
+                fruit.GetComponent<Rigidbody2D>().isKinematic = false;
+                fruit.layer = 6;
+                fruitExists = false;
+            }
         }
     }
 }
